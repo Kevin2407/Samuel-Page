0aec4bbb06c8c097cc5750e53cc83a2835033fe4
Kevin Martin <kevmartin2001@gmail.com>
2022-12-25 15:39:08 -0300

    rama de main limpiada

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Navegacion } from "./1.header/Navegacion";
-import Seccion1 from "./2.main/Seccion1";
+import Seccion1 from "./main/Seccion1";
 import './style.css';
 
 function App() {
   return (
     <>
-    <heather>
-      <Navegacion></Navegacion>
-    </heather>
     <body className="container centrar">
       <Seccion1></Seccion1>
     </body>
