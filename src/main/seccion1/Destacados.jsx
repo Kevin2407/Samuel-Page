cb49a66e463035ed3aa96bd7713314e6abeaafed
Kevin Martin <kevmartin2001@gmail.com>
2022-12-26 16:28:50 -0300

    diseño de seccion1 terminada

--- src/main/seccion1/Destacados.jsx
+++ src/main/seccion1/Destacados.jsx
@@ -8,10 +8,21 @@ function Destacados() {
       <div className="div-destacada">
         <img src={foto} alt="imagen de nota destacada" />
         <article className="texto-destacado">
-          
+          <small>Lunes 7 de Noviembre de 2022</small>
+          <h4>Titulo</h4>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nesciunt ab in magni enim, quisquam reiciendis, eveniet pariatur aut quia corporis quae, possimus voluptatem molestiae nemo perferendis a ea magnam.</p>  
+          </div>
         </article>
       </div>
       <div className="newsletter">
+        <article className="p-4 text-center">
+          <p className="lead"><strong>Suscribete ahora al NewsLetter de Samuel Von Arx</strong></p>
+          <p>Tené en tu email todos los contenidos de Samuel y empieza a odiar cada dia mas a Perón</p>
+          <p className="">Email</p>
+          <input type="text" />
+          <button className="btn">Suscribirse</button>
+        </article>
       </div>
     </section>
   );
