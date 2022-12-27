e0cd281f8df84308ba91b641ea7ac23d90c5ba1a
Kevin Martin <kevmartin2001@gmail.com>
2022-12-27 09:58:35 -0300

    articulo individual maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Seccion1 from "./main/seccion1/Seccion1";
+import Articulo from "./main/seccion2/Articulo";
 import './style.css';
 
 function App() {
   return (
     <div className="container centrar">
     <body>
-      <Seccion1></Seccion1>
+      <Articulo></Articulo>
     </body>
     </div>
   );
