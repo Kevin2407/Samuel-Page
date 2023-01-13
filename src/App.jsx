a54272359f031a96053785cd6d6667f7a04e394a
Kevin Martin <kevmartin2001@gmail.com>
2023-01-13 09:25:08 -0300

    agregado react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Navegacion } from "./header/Navegacion";
 import Seccion1 from "./main/seccion1/Seccion1";
 import ListaArticulos from "./main/seccion2/ListaArticulos";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
