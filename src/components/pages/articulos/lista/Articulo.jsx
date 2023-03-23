f552e5f685e9feee1f034ba2a762359d34666794
Kevin Martin <kevmartin2001@gmail.com>
2023-03-23 16:36:23 -0300

    resuelto el paso de html a texto en la lista de articulos

--- src/components/pages/articulos/lista/Articulo.jsx
+++ src/components/pages/articulos/lista/Articulo.jsx
@@ -1,96 +1,86 @@
-import React, { Component, useRef, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import foto from './default-seccion2.png';
 import './articulo.css';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import parse from 'html-react-parser';
 
 
 
 
-const Articulo = (props) => {
+class Articulo extends Component {
 
-    const [loading, setLoading] = useState(false);
-    
-
-
-
-    const borrar = (id) => {
-
-        const URL = process.env.REACT_APP_API_URL + '/' + id;
-
-        Swal.fire({
-            title: 'Estas seguro que quieres eliminar el producto?',
-            text: "No puedes recuperar el producto una vez eliminado",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Eliminar',
-            cancelButtonText: 'Cancelar'
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-
-                setLoading(true);
-                Swal.fire({
-                    title: 'Cargando...',
-                    allowOutsideClick: false,
-                    didOpen: () => {
-                        Swal.showLoading();
-                    }
-                }).then(() => {
-                    setLoading(false);
-                });
+    constructor(props) {
+        super(props);
+    }
 
 
 
-                try {
-                    const response = await fetch(URL, {
-                        method: 'DELETE',
-                        headers: {
-                            "Content-Type": "application/json"
+
+    render() {
+
+
+        const borrar = (id) => {
+
+            const URL = process.env.REACT_APP_API_URL + '/' + id;
+
+            Swal.fire({
+                title: 'Estas seguro que quieres eliminar el producto?',
+                text: "No puedes recuperar el producto una vez eliminado",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Eliminar',
+                cancelButtonText: 'Cancelar'
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+
+                    try {
+                        const response = await fetch(URL, {
+                            method: 'DELETE',
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        });
+
+                        if (response.status === 200) {
+                            // mostrar cartel de prod. eliminado
+                            Swal.fire(
+                                'Borrado!',
+                                'El producto ha sido borrado.',
+                                'success'
+                            )
+
+                            // actualizar los datos
+                            this.props.consultarAPI();
+
+
+                        } else {
+
+                            Swal.fire(
+                                'Se produjo un error!',
+                                'Intentelo nuevamente',
+                                'error'
+                            )
+
+
                         }
-                    });
 
-                    if (response.status === 200) {
-                        // mostrar cartel de prod. eliminado
-                        Swal.fire(
-                            'Borrado!',
-                            'El producto ha sido borrado.',
-                            'success'
-                        )
-
-                        // actualizar los datos
-                        props.consultarAPI();
-
-
-                    } else {
+                    } catch (error) {
+                        console.log(error);
 
                         Swal.fire(
                             'Se produjo un error!',
-                            'Intentelo nuevamente',
+                            'Intentelo en unos minutos',
                             'error'
                         )
-
-
                     }
-
-                } catch (error) {
-                    console.log(error);
-
-                    Swal.fire(
-                        'Se produjo un error!',
-                        'Intentelo en unos minutos',
-                        'error'
-                    )
                 }
-            }
-        })
+            })
 
 
-
-
+        }
 
 
         return (
@@ -104,17 +94,20 @@ const Articulo = (props) => {
                             {/* {width: 100%} */}
                         </div>
                         <section className='section-nota'>
-                            <Link to={`/articulo/${props.articulo._id}`} className='lead'>{props.articulo.titulo}</Link>
+                            <Link to={`/articulo/${this.props.articulo._id}`} className='lead'>{this.props.articulo.titulo}</Link>
                             <div>
-                                <p>{props.articulo.contenido.replace(/(<([^>]+)>)/ig, "")}</p>
+                                <p>{this.props.articulo.contenido.replace(/(<([^>]+)>)/ig, "").replace(/&nbsp;/g, "")}</p>
                             </div>
                         </section>
 
                     </div>
-                    <div className='text-center' style={{ display: "grid", alignContent: "center", width: '10%', justifyContent: 'flex-end' }}>
-                        <Link to={`/administracion/editar/${props.articulo._id}`} className='btn btn-warning text-light mb-1 botones-articulo'><FontAwesomeIcon icon={faPenToSquare} style={{ color: "#f5f5f5", }} /></Link>
-                        <button className='btn btn-danger mt-1 botones-articulo' onClick={() => borrar(props.articulo._id)}><FontAwesomeIcon icon={faTrash} style={{ color: "#f5f5f5", }} /></button>
-                    </div>
+                    {/* <div> */}
+                        <div className='text-center' style={{ display: "grid", alignContent: "center", width: '10%', justifyContent: 'flex-end' }}>
+                            <Link to={`/administracion/editar/${this.props.articulo._id}`} className='btn btn-warning text-light mb-1'>Editar</Link>
+                            <button className='btn btn-danger mt-1' onClick={() => borrar(this.props.articulo._id)}>Borrar</button>
+                        </div>
+
+                    {/* </div> */}
                 </article>
             </React.Fragment>
         );
