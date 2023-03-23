35781753227ab58967f72bc0a7e84d9a976662ee
Kevin Martin <kevmartin2001@gmail.com>
2023-03-23 16:21:39 -0300

    componente articulo pasado a funciton component, pero sin funcionar

--- src/components/pages/articulos/lista/Articulo.jsx
+++ src/components/pages/articulos/lista/Articulo.jsx
@@ -1,86 +1,96 @@
-import React, { Component } from 'react';
+import React, { Component, useRef, useEffect, useState } from 'react';
 import foto from './default-seccion2.png';
 import './articulo.css';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import parse from 'html-react-parser';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
 
 
 
-class Articulo extends Component {
+const Articulo = (props) => {
 
-    constructor(props) {
-        super(props);
-    }
-
+    const [loading, setLoading] = useState(false);
+    
 
 
 
-    render() {
+    const borrar = (id) => {
+
+        const URL = process.env.REACT_APP_API_URL + '/' + id;
+
+        Swal.fire({
+            title: 'Estas seguro que quieres eliminar el producto?',
+            text: "No puedes recuperar el producto una vez eliminado",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+
+                setLoading(true);
+                Swal.fire({
+                    title: 'Cargando...',
+                    allowOutsideClick: false,
+                    didOpen: () => {
+                        Swal.showLoading();
+                    }
+                }).then(() => {
+                    setLoading(false);
+                });
 
 
-        const borrar = (id) => {
 
-            const URL = process.env.REACT_APP_API_URL + '/' + id;
-
-            Swal.fire({
-                title: 'Estas seguro que quieres eliminar el producto?',
-                text: "No puedes recuperar el producto una vez eliminado",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Eliminar',
-                cancelButtonText: 'Cancelar'
-            }).then(async (result) => {
-                if (result.isConfirmed) {
-
-                    try {
-                        const response = await fetch(URL, {
-                            method: 'DELETE',
-                            headers: {
-                                "Content-Type": "application/json"
-                            }
-                        });
-
-                        if (response.status === 200) {
-                            // mostrar cartel de prod. eliminado
-                            Swal.fire(
-                                'Borrado!',
-                                'El producto ha sido borrado.',
-                                'success'
-                            )
-
-                            // actualizar los datos
-                            this.props.consultarAPI();
-
-
-                        } else {
-
-                            Swal.fire(
-                                'Se produjo un error!',
-                                'Intentelo nuevamente',
-                                'error'
-                            )
-
-
+                try {
+                    const response = await fetch(URL, {
+                        method: 'DELETE',
+                        headers: {
+                            "Content-Type": "application/json"
                         }
+                    });
 
-                    } catch (error) {
-                        console.log(error);
+                    if (response.status === 200) {
+                        // mostrar cartel de prod. eliminado
+                        Swal.fire(
+                            'Borrado!',
+                            'El producto ha sido borrado.',
+                            'success'
+                        )
+
+                        // actualizar los datos
+                        props.consultarAPI();
+
+
+                    } else {
 
                         Swal.fire(
                             'Se produjo un error!',
-                            'Intentelo en unos minutos',
+                            'Intentelo nuevamente',
                             'error'
                         )
+
+
                     }
+
+                } catch (error) {
+                    console.log(error);
+
+                    Swal.fire(
+                        'Se produjo un error!',
+                        'Intentelo en unos minutos',
+                        'error'
+                    )
                 }
-            })
+            }
+        })
 
 
-        }
+
+
 
 
         return (
@@ -94,20 +104,17 @@ class Articulo extends Component {
                             {/* {width: 100%} */}
                         </div>
                         <section className='section-nota'>
-                            <Link to={`/articulo/${this.props.articulo._id}`} className='lead'>{this.props.articulo.titulo}</Link>
+                            <Link to={`/articulo/${props.articulo._id}`} className='lead'>{props.articulo.titulo}</Link>
                             <div>
-                                <p>{this.props.articulo.contenido}</p>
+                                <p>{props.articulo.contenido.replace(/(<([^>]+)>)/ig, "")}</p>
                             </div>
                         </section>
 
                     </div>
-                    {/* <div> */}
-                        <div className='text-center' style={{ display: "grid", alignContent: "center", width: '10%', justifyContent: 'flex-end' }}>
-                            <Link to={`/administracion/editar/${this.props.articulo._id}`} className='btn btn-warning text-light mb-1'>Editar</Link>
-                            <button className='btn btn-danger mt-1' onClick={() => borrar(this.props.articulo._id)}>Borrar</button>
-                        </div>
-
-                    {/* </div> */}
+                    <div className='text-center' style={{ display: "grid", alignContent: "center", width: '10%', justifyContent: 'flex-end' }}>
+                        <Link to={`/administracion/editar/${props.articulo._id}`} className='btn btn-warning text-light mb-1 botones-articulo'><FontAwesomeIcon icon={faPenToSquare} style={{ color: "#f5f5f5", }} /></Link>
+                        <button className='btn btn-danger mt-1 botones-articulo' onClick={() => borrar(props.articulo._id)}><FontAwesomeIcon icon={faTrash} style={{ color: "#f5f5f5", }} /></button>
+                    </div>
                 </article>
             </React.Fragment>
         );
