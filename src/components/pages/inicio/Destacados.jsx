62934413719837b7bea9f18a0322766c85d1549b
Kevin Martin <kevmartin2001@gmail.com>
2023-03-31 18:40:27 -0300

    resuelto destacado

--- src/components/pages/inicio/Destacados.jsx
+++ src/components/pages/inicio/Destacados.jsx
@@ -1,31 +1,30 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './seccion1.css';
 import foto from './foto.jpg';
 
 function Destacados(props) {
 
-  const [imagenDestacada,setImagenDestacada] = useState(foto);
+  const [imagenDestacada, setImagenDestacada] = useState(props.destacado.imagen);
   const contenidoSinEtiquetas = props.destacado.contenido ? props.destacado.contenido.replace(/(<([^>]+)>)/ig, "").replace(/&nbsp;/g, "") : "";
 
-  useEffect(()=>{
-    let img = new Image();
-    img.src = props.destacado.imagen;
-    img.onload = () => {
-        setImagenDestacada(img.src);
-    }
-  },[])
+  
+  const img = new Image();
+  img.src = props.destacado.imagen;
+  img.onload = () => {
+    setImagenDestacada(img.src);
+  }
 
   return (
-      <div className="div-destacada">
-        <img src={imagenDestacada} alt="imagen de nota destacada" />
-        <article className="texto-destacado">
-          <small>{props.destacado.fecha}</small>
-          <h4>{props.destacado.titulo}</h4>
-          <div>
-            <p>{contenidoSinEtiquetas}</p>  
-          </div>
-        </article>
-      </div>
+    <div className="div-destacada">
+      <img src={imagenDestacada} alt="imagen de nota destacada" />
+      <article className="texto-destacado">
+        <small>{props.destacado.fecha}</small>
+        <h4>{props.destacado.titulo}</h4>
+        <div>
+          <p>{contenidoSinEtiquetas}</p>
+        </div>
+      </article>
+    </div>
   );
 }
 
